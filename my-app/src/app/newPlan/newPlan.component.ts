2f13d7780b5ca0ee7f6983a32ed6232fab8d029c
sashkaool <sasha.dubilo@gmail.com>
2021-07-30 15:18:44 +0300

    add

--- my-app/src/app/newPlan/newPlan.component.ts
+++ my-app/src/app/newPlan/newPlan.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core'
+import { Plans } from '../list/mock-list'
 
 @Component({
     selector: 'new-plan',
@@ -7,6 +8,10 @@ import { Component, Output, EventEmitter } from '@angular/core'
 })
 export class NewPlanComponent {
     isActive = 'basic';
+    list = Plans;
+    isSelect = ''
+    isOpen = '';
+    value = '';
 
     public select(id: string) {
         this.isActive = id
@@ -15,5 +20,17 @@ export class NewPlanComponent {
     @Output() onClose = new EventEmitter<boolean>()
     close(increased: any) {
         this.onClose.emit(increased)
+    }
+
+    public open(id: string) {
+        if (this.isOpen == '') {
+            this.isOpen = id
+        } else {
+            this.isOpen = ''
+        }
+    }
+
+    public selectItem(value: string) {
+        this.value = value
     }
 }
